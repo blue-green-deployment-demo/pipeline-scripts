952f9b2f3391de31f0d74230c1b14f622dc784a9
MANDEEP GOYAT <goyat.mandy@gmail.com>
2017-05-23 18:16:09 +0530

    updated jq installation steps.

--- installgocdagent.sh
+++ installgocdagent.sh
@@ -6,3 +6,4 @@ sudo usermod -aG docker,sudo go
 sudo sed -i -e 's,127.0.0.1,ec2-35-163-220-196.us-west-2.compute.amazonaws.com,g' /etc/default/go-agent
 sudo chmod -R 777 /var/lib/go-agent
 sudo /etc/init.d/go-agent restart
+sudo apt-get -y install jq
