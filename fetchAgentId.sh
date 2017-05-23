ab4d42f6533054f1b7352669f186492ad1ebb046
Ubuntu <ubuntu@ip-172-31-8-146.us-west-2.compute.internal>
2017-05-23 06:29:41 +0000

    added fetchgoAgent file

---
+++ fetchAgentId.sh
@@ -0,0 +1,6 @@
+var=`grep public_dns terraform.tfstate | awk -F ":" 'NR==1 {print $2}' | sed s/\"//g | sed s/\,//g`
+echo $var
+var2=ubuntu@${var}
+var3=$(echo $var2 | tr -d ' ')
+echo $var3
+scp -i ~/mandeepingpoc.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $var3:/var/lib/go-agent/config/guid.txt  .
