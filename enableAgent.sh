8240032b577843b87bcde56731f528ded8f8d8b4
MANDEEP GOYAT <goyat.mandy@gmail.com>
2017-05-23 18:59:38 +0530

    updated correct urls.

--- enableAgent.sh
+++ enableAgent.sh
@@ -2,7 +2,7 @@
 
 uuid=$1
 resource_input=$2
-agentDetails=`curl -k 'https://ec2-54-70-136-53.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json'| jq '{ "hostname": .hostname, "agent_config_state" : "Enabled" , "resources": ['\"$resource_input\"'], "environments" : ["Deploy"] }'`
+agentDetails=`curl -k 'https://ec2-35-163-220-196.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json'| jq '{ "hostname": .hostname, "agent_config_state" : "Enabled" , "resources": ['\"$resource_input\"'], "environments" : ["Deploy"] }'`
 echo $agentDetails > agentdetailstoUpdate.json
 
 curl -k 'https://ec2-35-163-220-196.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetailstoUpdate.json
